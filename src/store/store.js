c574721f42752f2f6e8d6884c990e6f84df33b4c
RadosavPanic <radosav.panic01@gmail.com>
2023-08-05 00:08:00 +0200

    Added redux-thunk middleware and updated redux-persist and store logic

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,23 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
-import logger from "redux-logger";
 import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { RootReducer } from "./root-reducer";
 
 const persistConfig = {
   key: "root",
   storage,
-  blacklist: ["user"],
+  whitelist: ["cart"],
 };
 
 const persistedReducer = persistReducer(persistConfig, RootReducer);
 
-const middleWares = [logger];
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean);
 const composeEnchancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(

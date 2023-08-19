ee8e450c08405b7606730752cad3e3aa20ff939e
RadosavPanic <radosav.panic01@gmail.com>
2023-08-19 11:41:04 +0200

    added tests for category.saga

---
+++ src/store/categories/__tests__/category.saga.test.js
@@ -0,0 +1,57 @@
+import { testSaga, expectSaga } from "redux-saga-test-plan";
+import { throwError } from "redux-saga-test-plan/providers";
+import { call } from "typed-redux-saga/macro";
+
+import {
+  fetchCategoriesAsync,
+  onFetchCategories,
+  categoriesSaga,
+} from "../category.saga";
+import { CATEGORIES_ACTION_TYPES } from "../category.types";
+import { getCategoriesAndDocuments } from "../../../utils/firebase/firebase.utils";
+import {
+  fetchCategoriesFailed,
+  fetchCategoriesSuccess,
+} from "../category.action";
+
+describe("Category Saga tests", () => {
+  test("categoriesSaga", () => {
+    testSaga(categoriesSaga)
+      .next()
+      .all([call(onFetchCategories)])
+      .next()
+      .isDone();
+  });
+
+  test("onFetchCategories", () => {
+    testSaga(onFetchCategories)
+      .next()
+      .takeLatest(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
+        fetchCategoriesAsync
+      )
+      .next()
+      .isDone();
+  });
+
+  test("fetchCategoriesAsync success", () => {
+    const mockCategoriesArray = [
+      { id: 1, name: "Category 1" },
+      { id: 2, name: "Category 2" },
+    ];
+
+    return expectSaga(fetchCategoriesAsync)
+      .provide([[call(getCategoriesAndDocuments), mockCategoriesArray]])
+      .put(fetchCategoriesSuccess(mockCategoriesArray))
+      .run();
+  });
+
+  test("fetchCategoriesAsync failed", () => {
+    const mockError = new Error("An error occurred");
+
+    return expectSaga(fetchCategoriesAsync)
+      .provide([[call(getCategoriesAndDocuments), throwError(mockError)]])
+      .put(fetchCategoriesFailed(mockError))
+      .run();
+  });
+});
